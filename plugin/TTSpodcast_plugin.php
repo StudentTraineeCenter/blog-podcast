1685a84cbf58ebccea415f2dcf2b9cd9b13c536f
ficakc <ficakc@seznam.cz>
2023-09-22 19:48:20 +0200

    Submit refreshes the page,but it shouldnt do that

--- plugin/TTSpodcast_plugin.php
+++ plugin/TTSpodcast_plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: TTSarticles 
  * Plugin URI: https://yourwebsite.com
  * Description: This plugin creates a button for all admins on a wordpress webste that allows them to make a podcast out of their article.
- * Version: 1.0
+ * Version: 1.3
  * Author: FilBlack
  * Author URI: https://yourwebsite.com
  */
@@ -14,14 +14,15 @@ function enqueue_my_plugin_scripts() {
     wp_enqueue_style('my-plugin-style', plugin_dir_url(__FILE__) . 'settings_style.css');
 
     // Enqueue JavaScript
-    wp_enqueue_script('my-plugin-script', plugin_dir_url(__FILE__) . 'setting_script.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('my-plugin-script', plugin_dir_url(__FILE__) . 'settings_script.js', array('jquery'), '1.0', true);
 }
 add_action('admin_enqueue_scripts', 'enqueue_my_plugin_scripts');
 
- // add admin menu
-function register_my_custom_menu_page() {
-    add_menu_page('My Settings Popup', 'Settings Popup', 'manage_options', 'my_settings_popup', 'my_settings_popup_callback');
+// Add meta box
+function add_my_custom_meta_box() {
+    add_meta_box('my_custom_meta_box', 'My Settings Popup', 'my_settings_popup_callback', 'post');
 }
+add_action('add_meta_boxes', 'add_my_custom_meta_box');
 
 // add page content
 function my_settings_popup_callback() {
@@ -33,6 +34,14 @@ function my_settings_popup_callback() {
             <h3>Settings</h3>
             <button id="closeSettingsPopup">Close</button>
             <!-- Your settings here -->
+            <form id="settingsForm" onsubmit="return false;">
+                <label for="language">Language:</label>
+                <input type="text" id="language" name="language">
+                <label for="setting2">Setting 2:</label>
+                <input type="text" id="setting2" name="setting2">
+                <!-- Submit button -->
+                <input type="submit" value="Save Settings">
+            </form>
         </div>
     </div>
     <?php
@@ -41,3 +50,66 @@ function my_settings_popup_callback() {
 
 
 
+
+function handle_ajax_request() {
+    $language = sanitize_text_field($_POST['language']);
+    $setting2 = sanitize_text_field($_POST['setting2']);
+    error_log("My function is being run.");
+    // Process data here
+    // Your Azure subscription key
+    $subscriptionKey = 'a8a1dcfbc0734c7094090da3535dc740';
+
+    // Your Azure endpoint
+    $endpoint = 'https://eastus.api.cognitive.microsoft.com/sts/v1.0/issuetoken';
+
+    // Set up cURL
+    $ch = curl_init($endpoint);
+
+    // SSML
+    $ssml = '<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" xml:lang="en-US">
+    <voice name="Microsoft Server Speech Text to Speech Voice (en-US, AriaNeural)">
+    <prosody rate="-10.00%">I want to tell you a secret.</prosody>
+    <break strength="x-strong"/><prosody rate="5.00%" pitch="+20.00%">I am not a human.</prosody>
+    </voice>
+    </speak>';
+
+    // Set up the headers
+    $headers = [
+        'Authorization: Bearer ' . $subscriptionKey,
+        'Content-Type: application/ssml+xml',
+        'X-Microsoft-OutputFormat: audio-16khz-64kbitrate-mono-mp3',
+        'User-Agent: SpeechFilda'
+    ];
+
+    // Set up cURL options
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $ssml); //Using this in the function
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // Execute the request and get the audio data
+    $result = curl_exec($ch);
+    // Error handling
+    /*if (curl_errno($ch)) {
+        // Failed, handle this case
+        wp_send_json_error(['message' => 'Failed to make API request: ' . curl_error($ch)]);
+    } else {
+        // Success, handle this case
+        wp_send_json_success(['message' => 'Podcast appended.']);
+    }*/
+    // Check for errors
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    } else {
+        // Save the audio data as an MP3 file
+        file_put_contents('C:\Users\ficak\Local Sites\ttspodcast\logs\php\output.mp3', $result);
+    }
+
+    // Close cURL
+    curl_close($ch);
+
+}
+add_action('wp_ajax_my_ajax_action', 'handle_ajax_request');
+
+
+// USE META BOXES INSTEAD OF ADMIN MENU
--- plugin/settings_script.js
+++ plugin/settings_script.js
@@ -2,12 +2,47 @@ document.addEventListener("DOMContentLoaded", function() {
     var showButton = document.getElementById("showSettingsPopup");
     var closeButton = document.getElementById("closeSettingsPopup");
     var popup = document.getElementById("settingsPopup");
+    console.log("DOMContentLoaded event fired")
+    // Check if buttons and popup exist in the DOM
+    if(showButton && closeButton && popup) {
+        showButton.addEventListener("click", function(event) {
+            event.preventDefault();
+            popup.classList.remove("hidden");
+        });
 
-    showButton.addEventListener("click", function() {
-        popup.classList.remove("hidden");
-    });
+        closeButton.addEventListener("click", function(event) {
+            event.preventDefault();
+            popup.classList.add("hidden");
+        });
+    } else {
+        console.error("Buttons or popup not found");
+    }
+    document.addEventListener("submit", function(event) {
+        if (event.target && event.target.id === 'settingsForm') {
+            event.preventDefault(); // prevent page reload
+            var form = document.getElementById("settingsForm");
+            
+            if(form) {
+                console.log("form is not none")
+                var formData = new FormData(form);
+                formData.append('action', 'my_ajax_action');  // Append action
 
-    closeButton.addEventListener("click", function() {
-        popup.classList.add("hidden");
+                fetch(ajaxurl, {
+                    method: 'POST',
+                    body: formData,
+                })
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data) {
+                    console.log(data);
+                })
+                .catch(function(error) {
+                    console.error("Error:", error);
+                });
+            } else {
+                console.error("Form not found");
+            }
+        }
     });
 });
