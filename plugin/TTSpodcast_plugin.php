e896eb976bab18b4853bdb871273c94047733515
ficakc <ficakc@seznam.cz>
2023-09-23 20:52:02 +0200

    Key not visible, working code lacking ssml modification

--- plugin/TTSpodcast_plugin.php
+++ plugin/TTSpodcast_plugin.php
@@ -57,7 +57,7 @@ function handle_ajax_request() {
     error_log("My function is being run.");
     // Process data here
     // Your Azure subscription key
-    $subscriptionKey = 'a8a1dcfbc0734c7094090da3535dc740';
+    $subscriptionKey = 'Nuh,uh';
 
     // Your Azure endpoint
     $endpoint1 = 'https://eastus.api.cognitive.microsoft.com/sts/v1.0/issuetoken';
